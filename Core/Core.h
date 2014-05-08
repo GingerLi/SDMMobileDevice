644227458c0c185f082c9f4fb4321b124b063c38
ginger <njupt88@163.com>
2014-05-08 15:44:50 +0800

    remove submodule

---
+++ Core/Core.h
@@ -0,0 +1,42 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Sam Marshall on 12/8/13.
+//  Copyright (c) 2013 Sam Marshall. All rights reserved.
+//
+
+#ifndef Core_Core_h
+#define Core_Core_h
+
+#pragma mark -
+#pragma mark Standard
+#include "Pointer.h"
+#include "Range.h"
+#include "Number.h"
+#include "Logging.h"
+#include "Attributes.h"
+#include "Buffer.h"
+#include "Array.h"
+#include "Endian.h"
+#include "File.h"
+
+#pragma mark -
+#pragma mark Geometry
+#ifdef __COREGEOMETRY__
+#include "GeometryMaths.h"
+#include "Vector.h"
+#include "Matrix.h"
+#endif
+
+#pragma mark -
+#pragma mark CoreFoundation
+#ifdef __COREFOUNDATION_COREFOUNDATION__ 
+#include "CFDictionaryAddition.h"
+#include "CFTypeAddition.h"
+#include "CFDataAddition.h"
+#include "CFStringAddition.h"
+#endif
+
+
+#endif
